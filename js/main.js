bdd13a4ac6000b2e60d68a252fa86b59b2354cda
yunkamb <91229388+yunkamb@users.noreply.github.com>
2021-11-15 20:36:58 -0300

    dom

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 // Desafio clase 08 DOM 
 
+let numero = Number(document.querySelector("#numeroEscogido").value);
+let año = Number(document.querySelector("#numeroEdad").value);
+let cumple = document.getElementById("numeroCumple");
+
+cumple.addEventListener('change', function () {
+    if (this.checked) {
+        cumple = 1771;
+    } else {
+        cumple = 1770;
+    }
+});
+
+
+function calcularResultado() {
+    console.log(`${numero}, ${año}, ${cumple}`)
