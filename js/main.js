a62dcc282ed28b8fec7b5042b1e29a948a909236
yunkamb <91229388+yunkamb@users.noreply.github.com>
2021-11-17 19:23:07 -0300

    Desafio 09 - Completo

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
 // Desafio clase 08 DOM 
 
-let cumple = document.querySelector("#numeroCumple");
 let edad = 0;
+
 let parrafo = document.querySelector("#resultadoMat")
- cumple.addEventListener("change", function(){
+
+let cumple = document.querySelector("#numeroCumple");
+cumple.addEventListener("change", function () {
     if (this.checked) {
         cumple = 1771
     } else {
         cumple = 1770
     }
-}) 
+})
 
 let defaultCumple = 1770
 
 function calcularResultado() {
     let numero = Number(document.querySelector("#numeroEscogido").value);
     let año = Number(document.querySelector("#numeroEdad").value);
-    if (cumple == "1771") {
-        console.log(`${numero}, ${año}, ${cumple}`)
-    } else {
-        console.log(`${numero}, ${año}, ${defaultCumple}`)
-    }
     calcular(numero, año, cumple);
     mostrarResultado();
-    
+
 }
 
 function calcular(numero, año, cumple) {
-    if (cumple=="1771") {
-    edad = (((numero * 2) + 5) * 50 + cumple) - año;
+    if (cumple == "1771") {
+        edad = (((numero * 2) + 5) * 50 + cumple) - año;
     } else {
         edad = (((numero * 2) + 5) * 50 + defaultCumple) - año;
     }
 }
 
 function mostrarResultado() {
-    parrafo.style.backgroundColor= "#c53b1c41";
-    parrafo.textContent =`El primer dígito de la siguiente cifra es el número que escogiste, y las siguientes dos cifras indican tu edad: ${edad}`;
+    parrafo.style.backgroundColor = "#c53b1c41";
+    parrafo.textContent = `El primer dígito de la siguiente cifra es el número que escogiste, y las siguientes dos cifras indican tu edad: ${edad}`;
 }
