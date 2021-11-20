644f07520d68ac582b29dda346730c9c1070cedc
yunkamb <91229388+yunkamb@users.noreply.github.com>
2021-11-20 16:07:20 -0300

    placeholder proyecto final

--- js/final.js
+++ js/final.js
@@ -1,3 +1,22 @@
+/* const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
+                  {  id: 2,  producto: "Fideo", precio: 70 },
+                  {  id: 3,  producto: "Pan"  , precio: 50},
+                  {  id: 4,  producto: "Flan" , precio: 100}];
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+
+//Almacenar producto por producto
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+// o almacenar array completo
+guardarLocal("listaProductos", JSON.stringify(productos));
+
+let datos = localStorage.getItem("listaProductos");
+let obj = JSON.parse(datos);
+
+ */
+
 class Usuario {
     constructor(nombre, apellido, edad) {
         this.nombre = nombre;
