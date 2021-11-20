5028cdd370f9f1d1035bb434d12e7204015a8ef8
yunkamb <91229388+yunkamb@users.noreply.github.com>
2021-11-20 18:08:55 -0300

    Segunda entrega final test

--- js/final.js
+++ js/final.js
@@ -1,45 +1,67 @@
+/* const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
+                  {  id: 2,  producto: "Fideo", precio: 70 },
+                  {  id: 3,  producto: "Pan"  , precio: 50},
+                  {  id: 4,  producto: "Flan" , precio: 100}];
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+
+//Almacenar producto por producto
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+// o almacenar array completo
+guardarLocal("listaProductos", JSON.stringify(productos));
+
+let datos = localStorage.getItem("listaProductos");
+let obj = JSON.parse(datos);
+
+ */
+
+let usuariosCreados = document.querySelector("#usuariosCreados");
+const arrayUsuarios = [];
+let numeroUsuario = 0;
+let i = 0;
+
 class Usuario {
-    constructor(nombre, apellido, edad) {
+    constructor(numero, nombre, apellido, edad) {
+        this.numero = numero;
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = parseInt(edad);
     }
 }
 
-const arrayUsuarios = [];
+const añadirUsuario = () => {
+    let numero = ++numeroUsuario;
+    let nombre = document.querySelector("#nombre").value;
+    let apellido = document.querySelector("#apellido").value;
+    let edad = Number(document.querySelector("#edad").value);
 
-let continuar = "s";
-
-while (continuar === "s") {
-    let nombre = prompt("Ingrese su nombre");
-    let apellido = prompt("Ingrese su apellido");
-    let edad = parseInt(prompt("Ingrese su edad"));
-
-    let nuevoUsuario = new Usuario(nombre, apellido, edad);
+    let nuevoUsuario = new Usuario(numero, nombre, apellido, edad);
     arrayUsuarios.push(nuevoUsuario);
-
-    continuar = prompt("¿Desea ingresar otro usuario? (s/n)");
+    console.log(arrayUsuarios);
+    storageTest();
 }
 
-console.log(arrayUsuarios);
+const storageTest = () => {
+    let guardarArray = JSON.stringify(arrayUsuarios);
+    localStorage.setItem("infoUsuarios", guardarArray);
+}
 
 
-let preview = prompt("¿Desea visualizar su información?(s/n)")
+const showJSON = document.querySelector("#json");
 
-if (preview == 'n') {
-    alert("Muchas gracias por utilizar nuestra pagina");
-} else {
-    for (let i = 0; i < arrayUsuarios.length; i++) {
-        alert(`Bienvenido ${arrayUsuarios[i].nombre} ${arrayUsuarios[i].apellido} de ${arrayUsuarios[i].edad} años de edad`)
+let mostrarUsuarios = document.querySelector("#json")
+
+mostrarUsuarios.addEventListener('click', function () {
+    archivoJSON = localStorage.getItem("infoUsuarios");
+    infoUsuarios = JSON.parse(archivoJSON);
+    for (let i = 0; i < infoUsuarios.length; i += 1) {
+        div = document.createElement('div');
+        div.style.backgroundColor = "#c53b1c41";
+        div.id = 'usuariosCreados';
+        div.className = 'usuarios';
+        div.innerHTML = `Usuario N°${infoUsuarios[i].numero} Nombre: ${infoUsuarios[i].nombre} Apellido: ${infoUsuarios[i].apellido} Edad: ${infoUsuarios[i].edad}`;
+        document.body.appendChild(div);
     }
-}
-
-function cantidadUsuarios(a) {
-    usuariosCreados = a;
-    alert(`Has creado ${usuariosCreados} usuarios.`);
-}
-
-cantidadUsuarios(arrayUsuarios.length);
-
-
-alert("Muchas gracias por utilizar nuestra pagina");
+});
