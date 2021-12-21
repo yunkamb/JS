3e7c9e1779c59d26ad3382b948909a947b01953e
yunkamb <91229388+yunkamb@users.noreply.github.com>
2021-12-21 01:08:38 -0300

    Juego adivinar color RGB

--- js/final.js
+++ js/final.js
@@ -1,64 +1,104 @@
-/* const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
-                  {  id: 2,  producto: "Fideo", precio: 70 },
-                  {  id: 3,  producto: "Pan"  , precio: 50},
-                  {  id: 4,  producto: "Flan" , precio: 100}];
+let numSquares = 9;
+let colors = [];
+let pickedColor;
 
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+let squares = document.querySelectorAll(".square");
+let colorDisplay = document.querySelector("#color-display");
+let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let modeButtons = document.querySelectorAll(".mode");
+let easyButton = document.querySelector(".mode");
 
-//Almacenar producto por producto
-for (const producto of productos) {
-    guardarLocal(producto.id, JSON.stringify(producto));
+init();
+
+function init() {
+    colorDisplay.textContent = pickedColor;
+    setupSquares();
+    setupMode();
+    reset();
 }
-// o almacenar array completo
-guardarLocal("listaProductos", JSON.stringify(productos));
 
-let datos = localStorage.getItem("listaProductos");
-let obj = JSON.parse(datos);
+resetButton.addEventListener("click", function () {
+    reset();
+});
 
- */
+function makeColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-class Usuario {
-    constructor(nombre, apellido, edad) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = parseInt(edad);
+function genRandomColors(num) {
+    let array = [];
+    for (let i = 0; i < num; i += 1) {
+        array.push(makeColor());
+    }
+    return array;
+}
+
+function changeColors(color) {
+    for (let i = 0; i < squares.length; i += 1) {
+        squares[i].style.backgroundColor = color;
+        h1.style.backgroundColor = color;
     }
 }
 
-const arrayUsuarios = [];
-
-let continuar = "s";
-
-while (continuar === "s") {
-    let nombre = prompt("Ingrese su nombre");
-    let apellido = prompt("Ingrese su apellido");
-    let edad = parseInt(prompt("Ingrese su edad"));
-
-    let nuevoUsuario = new Usuario(nombre, apellido, edad);
-    arrayUsuarios.push(nuevoUsuario);
-
-    continuar = prompt("¿Desea ingresar otro usuario? (s/n)");
+function chooseColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
 
-console.log(arrayUsuarios);
-
-
-let preview = prompt("¿Desea visualizar su información?(s/n)")
-
-if (preview == 'n') {
-    alert("Muchas gracias por utilizar nuestra pagina");
-} else {
-    for (let i = 0; i < arrayUsuarios.length; i++) {
-        alert(`Bienvenido ${arrayUsuarios[i].nombre} ${arrayUsuarios[i].apellido} de ${arrayUsuarios[i].edad} años de edad`)
+function setupSquares() {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", function () {
+            let clickedColor = this.style.backgroundColor;
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correcto!";
+                resetButton.textContent = "Jugar de vuelta";
+                changeColors(pickedColor);
+            } else {
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Intenta de nuevo";
+            }
+        });
     }
 }
 
-function cantidadUsuarios(a) {
-    usuariosCreados = a;
-    alert(`Has creado ${usuariosCreados} usuarios.`);
+function setupMode() {
+    for (let i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener("click", function () {
+            for (let i = 0; i < modeButtons.length; i += 1) {
+                modeButtons[i].classList.remove("selected");
+            }
+            this.classList.add("selected");
+            if (this.textContent === "Facil") {
+                numSquares = 3;
+            } else if (this.textContent === "Normal") {
+                numSquares = 6
+            } else {
+                numSquares = 9;
+            }
+            reset();
+        });
+    }
 }
 
-cantidadUsuarios(arrayUsuarios.length);
-
-
-alert("Muchas gracias por utilizar nuestra pagina");
+function reset() {
+    colors = genRandomColors(numSquares);
+    pickedColor = chooseColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "#d1d2cd";
+    resetButton.textContent = "Reiniciar colores";
+    messageDisplay.textContent = "";
+    for (let i = 0; i < squares.length; i += 1) {
+        if (colors[i]) {
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+}
